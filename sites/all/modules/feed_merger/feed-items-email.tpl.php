26651417eff45a72b153ac497c086b6dcacb3de9
Feng Tan <thomas1702@gmail.com>
2012-03-05 00:25:12 +0100

    theming: split feeds into distinct CSS divs

--- sites/all/modules/feed_merger/feed-items-email.tpl.php
+++ sites/all/modules/feed_merger/feed-items-email.tpl.php
@@ -1,14 +1,16 @@
-<div style="background-color:#d8d6d7;border:1px solid #c4c0c2;">
+<?php foreach($feeds as $feed): ?>
+<div style="background-color:#d8d6d7;margin:10px 0;border:1px solid #c4c0c2;">
   <div>
-    <h2 style="color:#252525;text-align:center;">Feeds for <?php print $user->name; ?></h2>
+    <h2 style="color:#252525;text-align:center;"><?php print $feed['title']; ?></h2>
   </div>
-  <?php foreach($feedItems as $feedItem): ?>
+  <?php foreach($feed['items'] as $feedItem): ?>
   <div style="background-color:#ffffff;margin:10px;padding:5px;border:1px solid #c4c0c2;">
     <span style="font-size:150%"><a href="<?php print $feedItem['link']; ?>"><?php print $feedItem['title']; ?></a></span>
-    <span style="color:#575c5d">(<?php print $feedItem['pubDate']; ?> - <i><?php print $feedItem['feedTitle']; ?></i>)</span>
+    <span style="color:#575c5d">(<?php print $feedItem['pubDate']; ?></span>
     <hr/>
     <span><?php print $feedItem['description']; ?></span>
   </div>
   <?php endforeach; ?>
 </div>
+<?php endforeach; ?>
 <a href="#">unsubscribe</a>
